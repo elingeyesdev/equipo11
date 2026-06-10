c2c5f85e73bb9baca480cb879784915140e10495
ifrancro <ifrancro@gmail.com>
2026-06-10 13:26:45 -0400

    Fix(PWA) Arreglo codigo muerto

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       strategies: 'injectManifest',
       srcDir: 'src',
       filename: 'sw.js',
-      registerType: 'autoUpdate',
+      registerType: 'prompt',
       injectManifest: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2,json}'],
         maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
