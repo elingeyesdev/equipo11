bc8c010491536ef528948df77d419d76ffd608b5
SergioAIS <sergioiporresiles@gmail.com>
2026-06-23 17:22:10 -0400

    Datos colchón

---
+++ Frontend/src/data/fallbackData.js
@@ -0,0 +1,11 @@
+export const FALLBACK_DATA = [
+  { id: 'lapaz',      name: 'La Paz',      latitude: -16.4897, longitude: -68.1193, data: { temperatura: 12, aqi: 65,  ica: 78, ruido: 72, humedad: 45 } },
+  { id: 'cochabamba', name: 'Cochabamba',  latitude: -17.3895, longitude: -66.1568, data: { temperatura: 24, aqi: 95,  ica: 82, ruido: 65, humedad: 30 } },
+  { id: 'santacruz',  name: 'Santa Cruz',  latitude: -17.7833, longitude: -63.1812, data: { temperatura: 30, aqi: 110, ica: 55, ruido: 78, humedad: 70 } },
+  { id: 'oruro',      name: 'Oruro',       latitude: -17.9624, longitude: -67.1061, data: { temperatura: 8,  aqi: 42,  ica: 88, ruido: 45, humedad: 35 } },
+  { id: 'potosi',     name: 'Potosí',      latitude: -19.5836, longitude: -65.7531, data: { temperatura: 5,  aqi: 38,  ica: 91, ruido: 40, humedad: 28 } },
+  { id: 'sucre',      name: 'Sucre',       latitude: -19.0353, longitude: -65.2592, data: { temperatura: 18, aqi: 55,  ica: 85, ruido: 58, humedad: 42 } },
+  { id: 'tarija',     name: 'Tarija',      latitude: -21.5355, longitude: -64.7296, data: { temperatura: 22, aqi: 48,  ica: 79, ruido: 52, humedad: 55 } },
+  { id: 'beni',       name: 'Trinidad',    latitude: -14.8333, longitude: -64.9000, data: { temperatura: 32, aqi: 78,  ica: 65, ruido: 62, humedad: 82 } },
+  { id: 'pando',      name: 'Cobija',      latitude: -11.0267, longitude: -68.7692, data: { temperatura: 28, aqi: 72,  ica: 60, ruido: 55, humedad: 88 } },
+];
